c36e2da3710d8a311135b06bfa5511a149c42789
JAMIL H. SPAIN <jamil.spain@us.ibm.com>
2019-02-18 17:01:07 -0500

    adding help menu for create command

--- bin/lbmesh.js
+++ bin/lbmesh.js
@@ -75,6 +75,13 @@ program
     }
 
 
+  })
+  .on('--help', function() {
+    console.log('  Examples:');
+    console.log();
+    console.log('    $ deploy exec sequential');
+    console.log('    $ deploy exec async');
+    console.log();
   });
 
 program
